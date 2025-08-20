053760b7b88a290d12c62cd8163d48f81ad78ea9
yefrysammir <yefrysammir@gmail.com>
2025-08-20 10:10:33 -0500

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME='sya-cache-v2';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.json'])))
+self.addEventListener("install", function (event) {
+  event.waitUntil(self.skipWaiting());
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
 });
